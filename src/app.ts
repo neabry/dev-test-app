60d766973750247a3e2d1d65dc0af224b6e2cd18
Karl Bryant <karlbryant98@gmail.com>
2021-05-12 11:54:17 +0100

    Add dotenv and server init

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from "express"
+import dotenv from "dotenv";
+import cors from "cors"
+import routes from "./routes"
+
+dotenv.config();
+
+const app = express()
+
+const PORT: string | number = process.env.PORT || 4000
+
+app.use(cors())
+app.use(routes)
+
+app.use(express.static("public"));
+
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
