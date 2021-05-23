28f71b06bf1099973f8aefa177cc9b8d21d0f68d
Karl Bryant <karlbryant98@gmail.com>
2021-05-23 18:44:52 +0100

    Add limit to query length

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,20 +2,38 @@ import { Router, Request, Response, NextFunction } from "express"
 import { getFlickrPhotos } from "../controllers/flickr";
 import { getYoutubeVideos } from "../controllers/youtube";
 
+// Set arbitrary length on query param limits
+const QUERY_LIMIT_LENGTH = 300
+
+
 const router: Router = Router()
 
 /**
- * Validate query parameters prior to passing to controllers
+ * Middleware validating query parameters prior to passing to controllers
  */
 router.use((req: Request, res: Response, next: NextFunction) => {
-  if (!req.query.q) {
+
+  const query = req.query.q as string | undefined;
+
+  // Query existence
+  if (!query) {
     res.status(400).json({
       status: 400,
-      error: `Missing "q" query parameter.`
+      error: `Missing "q" query parameter.`,
     });
-  } else {
-    next();
+    return;
   }
+
+  // Query length enforcement
+  if (query.length > QUERY_LIMIT_LENGTH) {
+    res.status(400).json({
+      status: 400,
+      error: `"q" query parameter exceeds enforced length.`,
+    });
+    return;
+  }
+
+  next();
 });
 
 router.get("/youtube", getYoutubeVideos);
