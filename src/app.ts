8277e6a1c31eb251f9a3a0a76df4b18a5eb1c397
Karl Bryant <karlbryant98@gmail.com>
2021-05-23 19:47:20 +0100

    Enable CORS

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/newline-after-import */
 /* eslint-disable import/first */
 import express from 'express';
+import cors from 'cors';
 
 // Parse .env file into environment variables
 import dotenv from 'dotenv';
@@ -12,6 +13,7 @@ const PORT: string | number = process.env.PORT || 4000;
 
 const app = express();
 
+app.use(cors());
 app.use('/api', routes);
 app.use(express.static('public'));
 
